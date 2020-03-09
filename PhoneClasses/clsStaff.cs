025ab33578ed5039dbe2c5ff24404c6612f839af
Muhammad Reman <P2577087@my365.dmu.ac.uk>
2020-03-09 17:22:06 +0000

    ClsStaff Updated

--- PhoneClasses/clsStaff.cs
+++ PhoneClasses/clsStaff.cs
@@ -8,5 +8,18 @@ namespace PhoneClasses
 {
     class clsStaff
     {
+
+        public string FirstName;
+        public string LastName;
+        public string HouseNumber;
+        public string Password;
+        public string Country;
+        public string PostCode;
+        public string StreetName;
+        public int Username;
+
+
+
+
     }
 }

529edd297ef217c7c169cd0c2572bb0cbb7bc8ee
KerenColaco03 <60647277+KerenColaco03@users.noreply.github.com>
2020-03-18 19:13:55 +0000

    Add files via upload

---
+++ PhoneFrontOffice/App_Code/clsProduct.cs
@@ -0,0 +1,271 @@
+﻿using ClassLibrary;
+using System;
+
+namespace PhoneClasses
+{
+    public class clsProduct
+    {
+        //private data member for the productID property
+        private Int32 mProductID;
+        //private data member for the brand
+        private string mBrand;
+        //private data member for the Colour
+        private string mColour;
+        //private data member for the productType
+        private string mProductType;
+        //private data member for the product Quanity
+        private Int32 mProductQuantity;
+        //private data member for the capacity
+        private string mCapacity;
+        //private data member for price 
+        private double mPrice;
+
+        public clsProduct()
+        {
+        }
+
+        public string Brand
+        {
+            get
+            {
+                //return the private data
+                return mBrand;
+            }
+
+            set
+            {
+                //set the value of teh private data member
+                mBrand = value;
+            }
+        }
+
+        public string Colour
+        {
+            get
+            {
+                //return the private data
+                return mColour;
+            }
+
+            set
+            {
+                //set the value of teh private data member
+                mColour = value;
+            }
+        }
+        public string ProductType
+        {
+            get
+            {
+                //return the private data
+                return mProductType;
+            }
+
+            set
+            {
+                //set the value of teh private data member
+                mProductType = value;
+            }
+        }
+        public int ProductID
+        {
+            get
+            {
+                //return the private data
+                return mProductID;
+            }
+
+            set
+            {
+                //set the value of teh private data member
+                mProductID = value;
+            }
+        }
+        public int ProductQuantity
+        {
+            get
+            {
+                //return the private data
+                return mProductQuantity;
+            }
+
+            set
+            {
+                //set the value of teh private data member
+                mProductQuantity = value;
+            }
+        }
+        public string Capacity
+        {
+            get
+            {
+                //return the private data
+                return mCapacity;
+            }
+
+            set
+            {
+                //set the value of teh private data member
+                mCapacity = value;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                //return the private data
+                return mPrice;
+            }
+
+            set
+            {
+                //set the value of teh private data member
+                mPrice = value;
+            }
+        }
+
+        public bool Find(int productID)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter fro the productID to search for 
+            DB.AddParameter("@ProductID", ProductID);
+            //execute the stored procedure 
+            DB.Execute("sproc_tblPhones_FilterByProductID");
+            //if no record is found (there should be either one or zero)
+            if (DB.Count == 1)
+            {
+                //copy the data from the databse to the private data members
+                mProductID = Convert.ToInt32(DB.DataTable.Rows[0]["ProductID"]);
+                mBrand = Convert.ToString(DB.DataTable.Rows[0]["Brand"]);
+                mProductType = Convert.ToString(DB.DataTable.Rows[0]["ProductType"]);
+                mCapacity = Convert.ToString(DB.DataTable.Rows[0]["Capacity"]);
+                mColour = Convert.ToString(DB.DataTable.Rows[0]["Colour"]);
+                mProductQuantity = Convert.ToInt32(DB.DataTable.Rows[0]["ProductQuantity"]);
+                mPrice = Convert.ToDouble(DB.DataTable.Rows[0]["Price"]);
+                //return taht everything worked ok
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return false indicating a problem
+                return false;
+            }
+        }
+
+        public string Valid(string brand, string productType, string capacity, string colour, string price, string productQuantity)
+        {
+            String Error = "";
+            //if the Brand is blank
+            if (brand.Length == 0)
+            {
+                //record the error
+                Error = Error + "The brand may not be blank : ";
+            }
+            //if the brand is greater than 30 charatcers 
+            if (brand.Length > 30)
+            {
+                //record the error
+                Error = Error + "The brand must be less than 30 characters";
+            }
+
+            if (productType.Length == 0)
+            {
+                //record the error
+                Error = Error + "The ProductType may not be blank";
+            }
+            //if the ProductType is greater than 30 characters
+            if (productType.Length > 30)
+            {
+                Error = Error + "The ProductType must be less than 30 characters";
+            }
+
+            if (capacity.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Capacity may not be blank";
+            }
+            //if the capacity is greater than 6 characters
+            if (capacity.Length > 6)
+            {
+                Error = Error + "The Capacity must be less than 6 characters";
+            }
+
+
+            if (colour.Length == 0)
+            {
+                //record the error
+                Error = Error + "The Colour may not be blank";
+            }
+            //if the colour is greater than 20 characters
+            if (colour.Length > 20)
+            {
+                Error = Error + "The Colour must be less than 20 characters";
+            }
+
+            //if the ProductQuantity is blank 
+            if (productQuantity.Length == 0)
+            {
+                //record an error
+                Error = Error + "The quantity may not be blank : ";
+            }
+            if (productQuantity.Length < 1 | productQuantity.Length > 6)
+            {
+                //record an error 
+                Error = Error + "The product quantity must be between 1 and 6 characters: ";
+            }
+            try
+            {
+                Int32 Temp;
+                Temp = Convert.ToInt32(productQuantity);
+                if (Temp < 1)
+                {
+                    Error = Error + "Quantity too short";
+                }
+                if (Temp > 6)
+                {
+                    Error = Error + "Product Quantity too long";
+                }
+            }
+            catch
+            {
+                Error = Error + "Please enter a valid Product Quantity";
+            }
+
+            //if the price is blank 
+            if (price.Length == 0)
+            {
+                //record an error
+                Error = Error + "The price may not be left blank";
+            }
+            if (price.Length < 1 | price.Length > 7)
+            {
+                //record an error
+                Error = Error + "The price must be between 1 and 7 characters";
+            }
+            try
+            {
+                Decimal Temp;
+                Temp = Convert.ToDecimal(price);
+                if (Temp < 1)
+                {
+                    Error = Error + "price is too short";
+                }
+                if (Temp > 7)
+                {
+                    Error = Error + "price is too long";
+                }
+            }
+            catch
+            {
+                Error = Error + "Please enter a valid price";
+            }
+
+
+            //return any error messages
+            return Error;
+        }
+    }
+}
+
---
+++ PhoneFrontOffice/App_Code/clsProductCollection.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary;
+
+namespace PhoneClasses
+{
+    public class clsProductCollection
+    {
+        //private data member for the list
+        List<clsProduct> mProductList = new List<clsProduct>();
+        //private data member thisProduct
+        clsProduct mThisProduct = new clsProduct();
+
+        //public property for the property list
+        public List<clsProduct> ProductList
+        {
+            get
+            {
+                //return the private data
+                return mProductList;
+            }
+            set
+            {
+                //set the private data 
+                mProductList = value;
+            }
+        }
+        //public property for count 
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mProductList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+
+
+        public clsProduct ThisProduct
+        {
+            get
+            {
+                //return the private data
+                return mThisProduct;
+            }
+            set
+            {
+                //set the private data
+                mThisProduct = value;
+            }
+        }
+
+        public int Add()
+        {
+            //adds a new record to the database based on the values of mThisProduct
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure 
+            DB.AddParameter("@Brand", mThisProduct.Brand);
+            DB.AddParameter("@Capacity", mThisProduct.Capacity);
+            DB.AddParameter("@Colour", mThisProduct.Colour);
+            DB.AddParameter("@Price", mThisProduct.Price);
+            DB.AddParameter("@ProductQuantity", mThisProduct.ProductQuantity);
+            DB.AddParameter("@ProductType", mThisProduct.ProductType);
+            //execute the query returning the priamry key value
+            return DB.Execute("sproc_tblPhones_Insert");
+        }
+
+        public void Delete()
+        {
+            //deletes the record pointed to by thisProduct
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure 
+            DB.AddParameter("@ProductID", mThisProduct.ProductID);
+            //execute the stored procedure 
+            DB.Execute("sproc_tblPhones_Delete");
+        }
+
+        public void Update()
+        {
+            //update an existing record based on the values of thisProduct
+            //connect to the database 
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure 
+            DB.AddParameter("@ProductID", mThisProduct.ProductID);
+            DB.AddParameter("@Brand", mThisProduct.Brand);
+            DB.AddParameter("@Capacity", mThisProduct.Capacity);
+            DB.AddParameter("@Colour", mThisProduct.Colour);
+            DB.AddParameter("@Price", mThisProduct.Price);
+            DB.AddParameter("@ProductQuantity", mThisProduct.ProductQuantity);
+            DB.AddParameter("ProductType", mThisProduct.ProductType);
+            //execute the stored procedure
+            DB.Execute("sproc_tblPhones_Update");
+        }
+
+        public void ReportByProductType(string ProductType)
+        {
+            //filters the records based on a full or partial code 
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //send the ProductType parameter to the database 
+            DB.AddParameter("@ProductType", ProductType);
+            //execute the stored procedure
+            DB.Execute("sproc_tblPhones_FilterByProductTypee");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+
+        //constructor for the class
+        public clsProductCollection()
+        {
+            //object for data connection 
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblPhones_SelectAll");
+            //populate the array list with the data table 
+            PopulateArray(DB);
+        }
+
+
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //populate the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount;
+            //get the count of records
+            RecordCount = DB.Count;
+            //clear the private array list
+            mProductList = new List<clsProduct>();
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create  a blank product
+                clsProduct AProduct = new clsProduct();
+                //read in the fields from the current record 
+                AProduct.ProductID = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductID"]);
+                AProduct.Brand = Convert.ToString(DB.DataTable.Rows[Index]["Brand"]);
+                AProduct.Capacity = Convert.ToString(DB.DataTable.Rows[Index]["Capacity"]);
+                AProduct.Colour = Convert.ToString(DB.DataTable.Rows[Index]["Colour"]);
+                AProduct.Price = Convert.ToDouble(DB.DataTable.Rows[Index]["Price"]);
+                AProduct.ProductQuantity = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductQuantity"]);
+                AProduct.ProductType = Convert.ToString(DB.DataTable.Rows[Index]["ProductType"]);
+                //add the record to the private data member
+                mProductList.Add(AProduct);
+                //point at the next record 
+                Index++;
+            }
+
+        }
+    }
+}
